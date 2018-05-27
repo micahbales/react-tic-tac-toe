22b7483611e48d38e3c0be3baf2cbaf369c1a765
Micah Bales <micahbales@gmail.com>
2018-05-27 13:55:39 -0400

    Allow Jump To Any Move in History

--- src/index.js
+++ src/index.js
@@ -51,13 +51,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1]
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
     const squares = current.squares.slice();
 
     if (calculateWinner(squares) || squares[i]) return;
@@ -67,13 +68,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
