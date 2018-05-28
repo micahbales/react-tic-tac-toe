158b8913ea6d7d1a47866f467817bb1a28142649
Micah Bales <micahbales@gmail.com>
2018-05-28 13:05:58 -0400

    Render Board Using Loops Rather Than Hard-Coded HTML
    To render arrays as HTML, we have to wrap them in curly braces.

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square 
+      key={i}
       value={this.props.squares[i]} 
       onClick= {() => this.props.onClick(i)}
       />
@@ -22,25 +23,25 @@ class Board extends React.Component {
   }
 
   render() {
+    // Render game board with 9 squares, 3 rows with 3 columns
+    let squareNum = 0;
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          [1,2,3].map((rowNum) => {
+            return (
+              <div className="board-row" key={rowNum}>
+                {
+                  [1,2,3].map((colNum) => {
+                    return this.renderSquare(squareNum++)
+                  })
+                }
+              </div>
+            )
+          })
+        }
       </div>
-    );
+    )
   }
 }
 
